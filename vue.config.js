57fbc0479f08a1204c603c3afa364ee4f73bc303
Volkov Nikolay <volkovnd@yandex.ru>
2021-10-17 05:02:41 +0300

    Some updates in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,34 @@
-// module.exports = {
-//   configureWebpack: (config) => {
-//     if (process.env.NODE_ENV === 'production') {
-//       const webpackConfig = config;
-//       webpackConfig.output.publicPath = `${process.cwd()}/dist/`;
-//     }
-//   },
-// };
+/** @type {import("@vue/cli-service").ProjectOptions} */
+module.exports = {
+  lintOnSave: false,
+  productionSourceMap: false,
+
+  css: {
+    sourceMap: process.env.NODE_ENV !== "production"
+  },
+
+  chainWebpack: (config) => {
+    config.performance.hints(config.mode === "production" ? "warning" : false);
+  },
+
+  configureWebpack: (config) => {
+    config.devtool = config.mode === "production" ? false : "source-map";
+  },
+
+
+  devServer: {
+    logLevel: "silent",
+    transportMode: "ws",
+    clientLogLevel: "none",
+    compress: true,
+    hot: true,
+    quiet: false,
+    before(app) {
+      app.use((req, res, next) => {
+        res.set("Access-Control-Allow-Origin", "*");
+
+        next();
+      });
+    },
+  },
+};
